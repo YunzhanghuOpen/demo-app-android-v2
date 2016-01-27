6a80f01a7eb31cb5211c82dfcc2ea94fa9b9bdc9
Bob <ityangb@163.com>
2016-01-27 17:08:30 +0800

    将腾讯地图更新到最新版本，使用新的方法获取逆地理位置 poi

--- app/src/main/java/io/rong/app/ui/activity/SOSOLocationActivity.java
+++ app/src/main/java/io/rong/app/ui/activity/SOSOLocationActivity.java
@@ -6,7 +6,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.Message;
-import android.util.Log;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,21 +15,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sea_monster.common.ParcelUtils;
+import com.tencent.lbssearch.TencentSearch;
+import com.tencent.lbssearch.httpresponse.BaseObject;
+import com.tencent.lbssearch.httpresponse.HttpResponseListener;
+import com.tencent.lbssearch.object.Location;
+import com.tencent.lbssearch.object.param.Geo2AddressParam;
+import com.tencent.lbssearch.object.result.Geo2AddressResultObject;
 import com.tencent.map.geolocation.TencentLocation;
 import com.tencent.map.geolocation.TencentLocationListener;
 import com.tencent.map.geolocation.TencentLocationManager;
 import com.tencent.map.geolocation.TencentLocationRequest;
-import com.tencent.tencentmap.mapsdk.map.GeoPoint;
+import com.tencent.mapsdk.raster.model.BitmapDescriptorFactory;
+import com.tencent.mapsdk.raster.model.GeoPoint;
+import com.tencent.mapsdk.raster.model.LatLng;
+import com.tencent.mapsdk.raster.model.MarkerOptions;
 import com.tencent.tencentmap.mapsdk.map.MapActivity;
-import com.tencent.tencentmap.mapsdk.map.MapController;
 import com.tencent.tencentmap.mapsdk.map.MapView;
-import com.tencent.tencentmap.mapsdk.map.PoiOverlay;
-import com.tencent.tencentmap.mapsdk.search.GeocoderSearch;
-import com.tencent.tencentmap.mapsdk.search.PoiItem;
-import com.tencent.tencentmap.mapsdk.search.ReGeocoderResult;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.apache.http.Header;
 
 import io.rong.app.DemoContext;
 import io.rong.app.R;
@@ -47,13 +52,11 @@ public class SOSOLocationActivity extends MapActivity implements
     LocationMessage mMsg;
     Handler mHandler;
     Handler mWorkHandler;
-    MapController mMapController = null;
     TextView mTitle;
     /**
      * 当前地图地址的poi
      */
     private HandlerThread mHandlerThread;
-
 
     private final static int RENDER_POI = 1;
     private final static int SHWO_TIPS = 2;
@@ -71,8 +74,9 @@ public class SOSOLocationActivity extends MapActivity implements
         mWorkHandler = new Handler(mHandlerThread.getLooper());
         mHandler = new Handler(this);
 
-        initView();
-
+        mMapView = (MapView) findViewById(android.R.id.widget_frame);
+        mTitle = (TextView) findViewById(android.R.id.title);
+        mButton = (Button) this.findViewById(android.R.id.button1);
 
         if (getIntent().hasExtra("location")) {
             mMsg = getIntent().getParcelableExtra("location");
@@ -82,49 +86,75 @@ public class SOSOLocationActivity extends MapActivity implements
             mButton.setVisibility(View.GONE);
         mButton.setOnClickListener(this);
 
-        mMapView.setBuiltInZoomControls(true); // 设置启用内置的缩放控件
+        mMapView.getUiSettings().setAnimationEnabled(true);// 地图缩放、平移动画开关
 
         if (mMsg == null) {
             GeoPoint point = new GeoPoint((int) (39.90923 * 1E6), (int) (116.397428 * 1E6)); // 用给定的经纬度构造一个GeoPoint，单位是微度
 
             mMapView.getController().setCenter(point);
-            mMapView.getController().setZoom(16);
+            mMapView.getMap().setZoom(16);
             mMapView.setOnTouchListener(this);
             TencentLocationRequest request = TencentLocationRequest.create();
             TencentLocationManager.getInstance(this).requestLocationUpdates(request, this);
 
         } else {
-            GeoPoint point = new GeoPoint((int) (mMsg.getLat() * 1E6), (int) (mMsg.getLng() * 1E6)); // 用给定的经纬度构造一个GeoPoint，单位是微度
+            LatLng latLng = new LatLng((mMsg.getLat()), (mMsg.getLng()));
+            mMapView.getMap().setZoom(16);
 
-            PoiItem poiItem = new PoiItem();
-            poiItem.name = mMsg.getPoi();
-            poiItem.point = point;
+            SOSOPoiItem sosoPoiItem = new SOSOPoiItem();
+            sosoPoiItem.setName(mMsg.getPoi());
+            sosoPoiItem.setLat((mMsg.getLat() * 1E6));
+            sosoPoiItem.setLng((mMsg.getLng() * 1E6));
 
-            mHandler.obtainMessage(RENDER_POI, poiItem).sendToTarget();
+            mHandler.obtainMessage(RENDER_POI, sosoPoiItem).sendToTarget();
             findViewById(android.R.id.icon).setVisibility(View.GONE);
-            mMapView.getController().setCenter(point);
-            mMapView.getController().setZoom(16);
+
+            mMapView.getMap().animateTo(latLng);
         }
 
     }
 
+    @Override
+    public boolean handleMessage(Message msg) {
+        if (msg.what == RENDER_POI) {
 
-    private void initView() {
-        mMapView = (MapView) findViewById(android.R.id.widget_frame);
-        mTitle = (TextView) findViewById(android.R.id.title);
-        mButton = (Button) this.findViewById(android.R.id.button1);
-        mMapController = mMapView.getController();
+            SOSOPoiItem poiItem = (SOSOPoiItem) msg.obj;
 
+            LatLng latLng = new LatLng(poiItem.getLat() / 1E6, poiItem.getLng() / 1E6);
+
+            mMapView.getMap().addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.red_location))
+                    .position(latLng)
+                    .draggable(true));
+
+        } else if (msg.what == SHWO_TIPS) {
+
+            SOSOPoiItem poiItem = (SOSOPoiItem) msg.obj;
+
+            mTitle.setText(poiItem.name);
+            mTitle.setVisibility(View.VISIBLE);
+
+            Uri uri = Uri
+                    .parse("http://apis.map.qq.com/ws/staticmap/v2").buildUpon()
+                    .appendQueryParameter("size", "240*240")
+                    .appendQueryParameter("key", "7JYBZ-4Y3W4-JMUU7-DJHQU-NOYH7-SRBBU")
+                    .appendQueryParameter("zoom", "16")
+                    .appendQueryParameter("center", poiItem.getLat() / 1E6
+                            + ","
+                            + poiItem.getLng() / 1E6).build();
+
+            mMsg = LocationMessage.obtain(poiItem.getLat() / 1E6,
+                    poiItem.getLng() / 1E6, poiItem.name, uri);
+
+        }
+        return false;
     }
 
-    PoiItem poiItem;
+
     @Override
     public void onLocationChanged(final TencentLocation tencentLocation,
                                   int code, String s) {
         if (TencentLocation.ERROR_OK == code) {
             Toast.makeText(this, "定位成功", Toast.LENGTH_SHORT).show();
-            Log.e("SOSOLocationActivity", "----------------onLocationChanged----dddd--");
-
 
             mHandler.post(new Runnable() {
                 @Override
@@ -132,14 +162,16 @@ public class SOSOLocationActivity extends MapActivity implements
                     GeoPoint point = new GeoPoint((int) (tencentLocation.getLatitude() * 1E6),
                             (int) (tencentLocation.getLongitude() * 1E6)); // 用给定的经纬度构造一个GeoPoint，单位是微度
                     mMapView.getController().setCenter(point);
-                    poiItem = new PoiItem();
-                    poiItem.name = tencentLocation.getAddress();
-                    poiItem.point = mMapView.getMapCenter();
 
-                    if(getIntent().hasExtra("location"))
-                        mHandler.obtainMessage(RENDER_POI, poiItem).sendToTarget();
+                    SOSOPoiItem sosoPoiItem = new SOSOPoiItem();
+                    sosoPoiItem.setName(tencentLocation.getAddress());
+                    sosoPoiItem.setLat((int) (tencentLocation.getLatitude() * 1E6));
+                    sosoPoiItem.setLng((int) (tencentLocation.getLongitude() * 1E6));
+
+                    if (getIntent().hasExtra("location"))
+                        mHandler.obtainMessage(RENDER_POI, sosoPoiItem).sendToTarget();
                     else
-                        mHandler.obtainMessage(SHWO_TIPS, poiItem).sendToTarget();
+                        mHandler.obtainMessage(SHWO_TIPS, sosoPoiItem).sendToTarget();
 
                 }
             });
@@ -154,7 +186,6 @@ public class SOSOLocationActivity extends MapActivity implements
 
     @Override
     public void onStatusUpdate(String s, int i, String s2) {
-        Log.e("SOSOLocationActivity", "----------------onStatusUpdate------");
     }
 
     @Override
@@ -183,56 +214,6 @@ public class SOSOLocationActivity extends MapActivity implements
 
     }
 
-    @Override
-    public boolean handleMessage(Message msg) {
-        if (msg.what == RENDER_POI) {
-            PoiItem poiItem = (PoiItem) msg.obj;
-
-            List<PoiItem> list = new ArrayList<PoiItem>();
-
-
-            list.add(poiItem);
-
-            mMapView.clearAllOverlays();
-
-            PoiOverlay myPoiOverlay = new PoiOverlay(null);
-            mMapView.addOverlay(myPoiOverlay);
-            myPoiOverlay.setPoiItems(list);
-            myPoiOverlay.showInfoWindow(0);
-
-
-            Uri uri = Uri
-                    .parse("http://apis.map.qq.com/ws/staticmap/v2").buildUpon().appendQueryParameter("size", "240*240")
-                    .appendQueryParameter("key", "7JYBZ-4Y3W4-JMUU7-DJHQU-NOYH7-SRBBU").appendQueryParameter("zoom", "16")
-                    .appendQueryParameter("center", mMapView.getMapCenter().getLatitudeE6() / 1E6 + "," + mMapView.getMapCenter()
-                            .getLongitudeE6() / 1E6).build();
-
-            Log.d("uri", uri.toString());
-
-            mMsg = LocationMessage.obtain(poiItem.point.getLatitudeE6() / 1E6,
-                    poiItem.point.getLongitudeE6() / 1E6, poiItem.name, uri);
-        } else if (msg.what == SHWO_TIPS) {
-
-            PoiItem poiItem = (PoiItem) msg.obj;
-
-            mTitle.setText(poiItem.name);
-            mTitle.setVisibility(View.VISIBLE);
-
-            Uri uri = Uri
-                    .parse("http://apis.map.qq.com/ws/staticmap/v2").buildUpon().appendQueryParameter("size", "240*240")
-                    .appendQueryParameter("key", "7JYBZ-4Y3W4-JMUU7-DJHQU-NOYH7-SRBBU").appendQueryParameter("zoom", "16")
-                    .appendQueryParameter("center", mMapView.getMapCenter().getLatitudeE6() / 1E6 + "," + mMapView.getMapCenter()
-                            .getLongitudeE6() / 1E6).build();
-
-            Log.e("tag", "-----uri---" + uri);
-            Log.e("tag", "-----uri--poiItem.name-" + poiItem.name);
-            mMsg = LocationMessage.obtain(poiItem.point.getLatitudeE6() / 1E6,
-                    poiItem.point.getLongitudeE6() / 1E6, poiItem.name, uri);
-
-
-        }
-        return false;
-    }
 
     POISearchRunnable mLastSearchRunnable;
 
@@ -252,8 +233,6 @@ public class SOSOLocationActivity extends MapActivity implements
             case MotionEvent.ACTION_UP:
                 mLastSearchRunnable = new POISearchRunnable();
                 mWorkHandler.post(new POISearchRunnable());
-
-//                mHandler.obtainMessage(SHWO_TIPS, poiItem).sendToTarget();
                 break;
             default:
                 break;
@@ -266,27 +245,121 @@ public class SOSOLocationActivity extends MapActivity implements
     private class POISearchRunnable implements Runnable {
 
         public void run() {
-            try {
-                GeocoderSearch geocodersearcher = new GeocoderSearch(SOSOLocationActivity.this);
-                GeoPoint geoRegeocoder = new GeoPoint(mMapView.getMapCenter().getLatitudeE6(), mMapView.getMapCenter().getLongitudeE6());
-                ReGeocoderResult regeocoderResult = geocodersearcher.searchFromLocation(geoRegeocoder);
 
-                if (regeocoderResult == null || regeocoderResult.poilist == null || regeocoderResult.poilist.size() == 0)
+            try {
+                float lat = (float) mMapView.getMap().getMapCenter().getLatitude();
+                float lng = (float) mMapView.getMap().getMapCenter().getLongitude();
+
+                Location location = new Location(lat, lng);
+                if (location == null)
                     return;
 
-                PoiItem poiItem = new PoiItem();
-                poiItem.name = regeocoderResult.poilist.get(0).name;
-                poiItem.point = mMapView.getMapCenter();
+                TencentSearch tencentSearch = new TencentSearch(SOSOLocationActivity.this);
+                //还可以传入其他坐标系的坐标，不过需要用coord_type()指明所用类型
+                //这里设置返回周边poi列表，可以在一定程度上满足用户获取指定坐标周边poi的需求
+                Geo2AddressParam geo2AddressParam = new Geo2AddressParam().
+                        location(location).get_poi(true);
 
-                if(getIntent().hasExtra("location"))
-                    mHandler.obtainMessage(RENDER_POI, poiItem).sendToTarget();
-                else
-                    mHandler.obtainMessage(SHWO_TIPS, poiItem).sendToTarget();
+                tencentSearch.geo2address(geo2AddressParam, new HttpResponseListener() {
 
+                    @Override
+                    public void onSuccess(int arg0, Header[] arg1, BaseObject arg2) {
+                        if (arg2 == null)
+                            return;
+
+                        Geo2AddressResultObject obj = (Geo2AddressResultObject) arg2;
+
+                        SOSOPoiItem sosoPoiItem = new SOSOPoiItem();
+                        sosoPoiItem.setName(obj.result.address);
+
+                        sosoPoiItem.setLat((int) (mMapView.getMap().getMapCenter().getLatitude() * 1E6));
+                        sosoPoiItem.setLng((int) (mMapView.getMap().getMapCenter().getLongitude() * 1E6));
+
+                        if (getIntent().hasExtra("location"))
+                            mHandler.obtainMessage(RENDER_POI, sosoPoiItem).sendToTarget();
+                        else
+                            mHandler.obtainMessage(SHWO_TIPS, sosoPoiItem).sendToTarget();
+                    }
+
+                    @Override
+                    public void onFailure(int arg0, Header[] arg1, String arg2, Throwable arg3) {
+
+                    }
+                });
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
+    }
+
+    private class SOSOPoiItem implements Parcelable {
+
+        private String name;
+
+        private double lat;
+
+        private double lng;
+
+        public SOSOPoiItem() {
+
+        }
+
+        public final Creator<SOSOPoiItem> CREATOR = new Creator<SOSOPoiItem>() {
+
+            @Override
+            public SOSOPoiItem createFromParcel(Parcel source) {
+
+                return new SOSOPoiItem(source);
+            }
+
+            @Override
+            public SOSOPoiItem[] newArray(int size) {
+
+                return new SOSOPoiItem[size];
+            }
+        };
+
+        @Override
+        public int describeContents() {
+            return 0;
+        }
+
+        private SOSOPoiItem(Parcel in) {
+            name = ParcelUtils.readFromParcel(in);
+            lat = Double.parseDouble(ParcelUtils.readFromParcel(in));
+            lng = Double.parseDouble(ParcelUtils.readFromParcel(in));
+        }
+
+        @Override
+        public void writeToParcel(Parcel parcel, int i) {
+            ParcelUtils.writeToParcel(parcel, name);
+            ParcelUtils.writeToParcel(parcel, lat);
+            ParcelUtils.writeToParcel(parcel, lng);
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public double getLat() {
+            return lat;
+        }
+
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+
+        public double getLng() {
+            return lng;
+        }
+
+        public void setLng(double lng) {
+            this.lng = lng;
+        }
     }
 }
